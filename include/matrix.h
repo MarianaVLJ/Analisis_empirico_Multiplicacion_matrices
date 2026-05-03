aeb015e2b35e918aaf43153457368c41c8fa30e8
MarianaVLJ <mluisj@unsa.edu.pe>
2026-05-03 01:20:40 -0500

    defino el struct Matrix y la macro de acceso MAT, el struct guarda el tamaño n y los datos en un array plano, la macro MAT(m,i,j) hace mas legible el acceso a cada elemento sin tener que escribir el calculo del indice a mano cada vez

---
+++ include/matrix.h
@@ -0,0 +1,16 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+typedef struct {
+  int n;
+  double *data;
+} Matrix;
+
+#define MAT(m, i, j) ((m)->data[(i) * (m)->n + (j)])
+
+Matrix *matrix_create(int n);
+void matrix_free(Matrix *m);
+void matrix_fill_random(Matrix *m, unsigned int seed);
+void matrix_print(const Matrix *m, int max_rows);
+
+#endif
