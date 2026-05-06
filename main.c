ffc54d611949b10b5325d86f5384013534df3a3b
MarianaVLJ <mluisj@unsa.edu.pe>
2026-05-06 00:59:58 -0500

    se modifica main.c para probar multiply_std con dos matrices de 4x4 e imprimir el resultado

--- main.c
+++ main.c
@@ -5,11 +5,25 @@ int main(void) {
   int n = 4;
 
   Matrix *A = matrix_create(n);
+  Matrix *B = matrix_create(n);
+
   matrix_fill_random(A, 42);
+  matrix_fill_random(B, 123);
+
+  Matrix *C = multiply_std(A, B);
 
   printf("Matriz A (%dx%d):\n", n, n);
   matrix_print(A, n);
 
+  printf("Matriz B (%dx%d):\n", n, n);
+  matrix_print(B, n);
+
+  printf("Resultado C = A x B:\n");
+  matrix_print(C, n);
+
   matrix_free(A);
+  matrix_free(B);
+  matrix_free(C);
+
   return 0;
 }
