bec93b728045241aaf9254e5cacd7e9875b964ae
MarianaVLJ <mluisj@unsa.edu.pe>
2026-05-06 23:59:40 -0500

    declaro matrix_add, matrix_sub y matrix_get_quadrant en matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -12,6 +12,11 @@ Matrix *matrix_create(int n);
 void matrix_free(Matrix *m);
 void matrix_fill_random(Matrix *m, unsigned int seed);
 void matrix_print(const Matrix *m, int max_rows);
+
+Matrix *matrix_add(const Matrix *A, const Matrix *B);
+Matrix *matrix_sub(const Matrix *A, const Matrix *B);
+Matrix *matrix_get_quadrant(const Matrix *A, int row, int col);
+
 Matrix *multiply_std(const Matrix *A, const Matrix *B);
 Matrix *multiply_strassen(const Matrix *A, const Matrix *B);
 
